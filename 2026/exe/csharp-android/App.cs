574b6680fc51f9f1eb24647b1d7bfff43719252e
diolamar <79411060+diolamar@users.noreply.github.com>
2026-05-10 10:02:28 +0800

    Delete 2026/exe/csharp-android directory

--- 2026/exe/csharp-android/App.cs
+++
@@ -1,9 +0,0 @@
-namespace CsvCrudSample.Android;
-
-public sealed class App : Application
-{
-    protected override Window CreateWindow(IActivationState? activationState)
-    {
-        return new Window(new NavigationPage(new MainPage()));
-    }
-}
